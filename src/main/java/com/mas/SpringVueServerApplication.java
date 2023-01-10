0f31c544c2b8809c918a8a81ebd997732f220741
Mekontso Anderson Saa <mekontsoanderson@gmail.com>
2023-01-10 12:43:13 +0100

    Initial commit

---
+++ src/main/java/com/mas/SpringVueServerApplication.java
@@ -0,0 +1,13 @@
+package com.mas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVueServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mas/SpringVueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
